97027685da334aef337e57110948ab6fb188abdd
laaksni <niko.laaksonen@hotmail.com>
2024-04-01 23:57:38 +0300

    Tehty React Router sovellus

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import { Link, Outlet } from 'react-router-dom';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export default function App() {
+  return (
+    <Container maxWidth="xl">
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            React Router
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <Outlet />
+    </Container>
+  );
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,6 @@
+export default function Home() {
+
+    return (
+      <h1>Homepage this is!</h1>
+    );
+  }
